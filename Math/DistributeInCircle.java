7e857f824f97844839b682b56429c707d546f833
Bijender Rawat <70880418+BijenderRawat@users.noreply.github.com>
2023-08-17 21:05:09 +0530

    last commit of the day

---
+++ Math/DistributeInCircle.java
@@ -0,0 +1,13 @@
+/*
+ * Easy level q asked in Goldman Sachs
+ * 
+ * You are given A number of packages, B is the size and C is the starting position
+ * 
+ * return the position where the last package will be delivered
+ */
+
+public class DistributeInCircle {
+    public int solve(int A, int B, int C) {
+        return (A + C - 1) % B;
+    }
+}
