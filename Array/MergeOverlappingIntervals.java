bcbe2b99c06ec8d084b7c38ebbfe6d108a568c3c
Bijender Rawat <70880418+BijenderRawat@users.noreply.github.com>
2023-08-17 12:45:39 +0530

    commit of the day

---
+++ Array/MergeOverlappingIntervals.java
@@ -0,0 +1,48 @@
+/*
+ * Medium level q asked in Google, Amazon
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
+
+public class MergeOverlappingIntervals {
+    public static ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        Collections.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+        ArrayList<Interval> ans = new ArrayList<>();
+        for (Interval i : intervals) {
+            if (ans.isEmpty())
+                ans.add(i);
+            else {
+                Interval temp = ans.get(ans.size() - 1);
+                if (temp.start > i.end) {
+                    ans.add(i);
+                } else if (temp.end >= i.start) {
+                    ans.remove(ans.size() - 1);
+                    ans.add(new Interval(Math.min(i.start, temp.start), Math.max(i.end, temp.end)));
+                } else
+                    ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
