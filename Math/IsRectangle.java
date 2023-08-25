05f57d1030d052d272660e89aeb50e4c79809d90
Bijender Rawat <70880418+BijenderRawat@users.noreply.github.com>
2023-08-25 12:06:20 +0530

    2 commits today

---
+++ Math/IsRectangle.java
@@ -0,0 +1,17 @@
+/*
+ * Easy q to check where from the given lengths, a rectangle can be formed or not?
+ */
+
+public class IsRectangle {
+    public int solve(int A, int B, int C, int D) {
+        if (A == B && B == C && C == D)
+            return 1;
+        if (A == B && C == D)
+            return 1;
+        if (A == C && B == D)
+            return 1;
+        if (A == D && B == C)
+            return 1;
+        return 0;
+    }
+}
---
+++ Math/PrimeSum.java
@@ -0,0 +1,38 @@
+/* 
+ * Easy q
+ * 
+ * Given an even number as input
+ * 
+ * return the 2 prime number, x and y whose sum is = that number
+ * 
+ * return lexicographically smaller prime numbers
+ */
+
+import java.util.ArrayList;
+
+/*
+ * In this question, the line "return lexicographically smaller primes" is of no use
+ * 
+ * because answer of this question would be the very first 2 primes
+ */
+
+public class PrimeSum {
+    public boolean isPrime(int A) {
+        for (int i = 2; i <= Math.sqrt(A); i++)
+            if (A % i == 0)
+                return false;
+        return true;
+    }
+
+    public ArrayList<Integer> primesum(int A) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 2; i <= A / 2; i++) {
+            if (isPrime(i) && isPrime(A - i)) {
+                ans.add(i);
+                ans.add(A - i);
+                break;
+            }
+        }
+        return ans;
+    }
+}
